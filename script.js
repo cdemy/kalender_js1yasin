2d84f1fdb5125a2d67bb2dba61f3df5da544d82b
nasenloch63 <yasin.aissani@gmx.de>
2026-07-31 07:32:56 +0200

    wochentage klassifiziert

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ var y = today.getFullYear();
 var dmy = d + "." + m + "." + y;
 
 
-let headline = document.getElementById("main-headline");
-headline.textContent = "Neuer, cooler Titel!";
+const heute = new Date();
+console.log(heute);
+
+const tag = heute.getDate();
+const monat = heute.getMonth() + 1;
+const jahr = heute.getFullYear();
+
+console.log(tag + "." + monat + "." + jahr);
+const wochentage = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+const wochentag = heute.getDay();
+console.log(wochentage[wochentag]);

3fe73ceab45e8181abaa81bfcc50e9817cefcb76
nasenloch63 <yasin.aissani@gmx.de>
2026-07-31 09:01:54 +0200

    cc

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var today = new Date();
-var d = today.getDate();
-var m = today.getMonth()
-var y = today.getFullYear();
+var today = new Date();  // Aktuelles Datum
+var d = today.getDate(); // Aktueller Tag
+var m = today.getMonth() // Aktueller Monat (0-11)
+var y = today.getFullYear(); // Aktuelles Jahr
 
-var dmy = d + "." + m + "." + y;
-
+headline.textContent = "Kalenderblatt vom " + d + "." + (m + 1) + "." + y;
 
 const heute = new Date();
 console.log(heute);
